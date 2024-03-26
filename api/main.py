e1642094c411995e9393ad0854e6311dfae817dd
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2024-03-26 16:15:59 +0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -38,8 +38,8 @@ def trending_characters():
     return jsonify(trending), 200, {'Content-Type': 'application/json; charset=utf-8'}
     
 @app.route('/api/rec')
-def trending_characters():
-    trending = client.character.recommended()
+def rec_characters():
+    rec = client.character.recommended()
 
     return jsonify(trending), 200, {'Content-Type': 'application/json; charset=utf-8'}
     

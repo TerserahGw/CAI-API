34e610289f5d896c02ed755f2c23bce61e5836c0
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2024-03-27 09:55:23 +0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -6,6 +6,10 @@ app = Flask(__name__, template_folder='.')
 client = PyCAI('29422450f9ebdf864bb798a6f9796cdab019d9f1')
 
 @app.route('/')
+def welcome()
+    return render_template('index.html')
+
+@app.route('/api')
 def home():
     return render_template('home.html')
 
